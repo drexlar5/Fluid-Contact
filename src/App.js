d29815c26bd22e2fa6551b85ae2e6ee4fe7f3fee
Agboola Michael <drexlar37@gmail.com>
2018-11-08 23:33:13 +0100

    Design contact page body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Nav from './containers/Nav'
+import Body from './containers/Body'
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Nav/>
+        <Nav/>      
+        <Body/>
       </div>
     );
   }
