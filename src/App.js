12c678ed48732f7a1b842ba9b9971caa5e8839d0
Agboola Michael <drexlar37@gmail.com>
2018-11-09 09:57:33 +0100

    Design Add contact modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Nav from './containers/Nav'
 import Body from './containers/Body'
+import Add from './containers/AddContact'
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Nav/>      
-        <Body/>
+        <Nav />
+        <Body />
+        <Add />
       </div>
     );
   }
